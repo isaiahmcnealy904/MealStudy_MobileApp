326353ed1d97d703ee224bb07a6497a4112d01be
Isaiah <isaiah_mcnealy_o@lilly.com>
2020-08-13 10:10:06 -0400

    Final Project Submit

---
+++ MDIT_MEALSTUDY_APP/Model/GlucoseThresholdModel.swift
@@ -0,0 +1,14 @@
+//
+//  GlucoseThresholdModel.swift
+//  MDIT_MEALSTUDY_APP
+//
+//  Created by Isaiah McNealy on 8/5/20.
+//  Copyright © 2020 Pradeep Mysore. All rights reserved.
+//
+
+import Foundation
+
+struct GlucoseThresholdModel:Codable {
+    var low: String
+    var high: String
+}
